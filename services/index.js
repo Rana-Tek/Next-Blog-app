e9f5c0c82b8254de611fa23dfb90a88111b92bcc
Rana-Tek <ranatekbas122@gmail.com>
2024-06-12 17:13:35 +0300

    ekstra method unwork

--- services/index.js
+++ services/index.js
@@ -1,10 +1,11 @@
-import axios from "axios";
+ import axios from "axios";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const fetchPosts = async() => {
-  const endpoint = 'https://api-eu-west-2.hygraph.com/v2/clwss8lq4024707w6bi76p10y/master';
+
+  const endpoint ='https://api-eu-west-2.hygraph.com/v2/clwss8lq4024707w6bi76p10y/master';
   
 
 
@@ -44,7 +45,7 @@ try {
     query: query
   }, {
     headers:{
-      'authorization': ` token ${process.env.AUTH_TOKEN }`,
+      'authorization': `eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjozLCJpYXQiOjE3MTgxODg1NTIsImF1ZCI6WyJodHRwczovL2FwaS1ldS13ZXN0LTIuaHlncmFwaC5jb20vdjIvY2x3c3M4bHE0MDI0NzA3dzZiaTc2cDEweS9tYXN0ZXIiLCJtYW5hZ2VtZW50LW5leHQuZ3JhcGhjbXMuY29tIl0sImlzcyI6Imh0dHBzOi8vbWFuYWdlbWVudC1ldS13ZXN0LTIuaHlncmFwaC5jb20vIiwic3ViIjoiZDE1YjIxZDEtZTM1Yy00YzczLThjOGMtNDA0ZTVmY2ViYWU5IiwianRpIjoiY2x4YnAzbnFoM2thNTA4bW44YWZ1MzNlOCJ9.Me6TTagbPQWVaCtIyMgRindC_PAiT1F4EaldN0tjz-hFQVEBUxtYB54ZmyqxcuDiIiAxqVDqvj6G92Ddbf7NeVe-3ntQJ8CihejwMsEP2OTRv1tc8vjVw2VVsPvingXc00aT6Kq-0Rx1EyXUn8wfdk-dYk9z8GHLZX2oBxo0IuuQ1QCvpRmfY27erqt5mR5CBonYKcL7VDC_lQjiLGjfs1aR8CUuEzTHhhoVs18m2VsqUwXapHTu4fRmAKXidEEINf9Z9CKpPZ23N-YRSY-MgV4qr0DfvgiXDwNtmMK7903n2OhYinOt7Z6r_qtsVUZC9onzCgmb8RkZL62H-XSX7LW5XbWtFDJLD9auxd3_03angTG7rTdi3rwgvYbYENi9Q2s3Knzf32x6sBA2GP-XCYq16h5cV5OoiXZtRxm1aUEHUP2D8YxVTd7y2aVwa8x-p5NTTCRYLHmbeG5lGX4TwA7iM7swdT4X8KOPpdP_WWl-lhsG3t5E7_g_zEYIdGY7CO2E-gewIrpsyB9m9zzJa1OqA9sTbRcfrrbyWyEGUgysk1Pwv0CaNeSQG28AjImt4zyMexMOac6GGK-sw4vjZyTTQUieXQLlpYwGRrD5Dox9pvevXtvLtcicsfQLUD92tuG6FzDT4dZCvsITJ3bt1qlb8AR0aZR9JiKG7GrN1gI`,
     }
   });
   return response.data;
@@ -56,3 +57,4 @@ try {
 };
 
 export default fetchPosts;
+
--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,18 @@ import { PostCard, Categories, PostWidget } from "../components";
 import { useEffect, useState } from "react";
 import fetchPosts from "../../services";
 
+
+
 export default function Home() {
+
+
   const [posts, setPosts] = useState([]);
   const [isLoading, setLoading] = useState(true);
   useEffect(() => {
     const fetchData = async () => {
-      const data = await fetchPosts();
+      {/*const data = await fetchPosts();*/}
+      const response = await fetch('https://api-eu-west-2.hygraph.com/v2/clwss8lq4024707w6bi76p10y/master');
+      const data = await response.json();
       if (data) {
         setPosts(data.posts);
         setLoading(false)
@@ -18,14 +24,15 @@ export default function Home() {
     };
     fetchData();
   }, []);
-
-  {/*if (!posts.length) {
+  
+{/*
+  if (!posts.length) {
     return <div>LOADING...</div>;
   }
-
-  */}
+*7.}
+  
   if (isLoading) return <p>Loading...</p>
-  if (!data) return <p>No profile data</p>
+ {/* if (!posts.length) return <p>No profile data</p> */}
 
   return (
     <div className="container mx-auto px-10 mb-8">
@@ -35,10 +42,10 @@ export default function Home() {
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post) => (
+          {posts ? posts.map((post) => (
           
-            <PostCard post={post.node} key={post.title} />
-          ))}
+            <PostCard post={post.node} key={post.title} /> 
+          )):null}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
